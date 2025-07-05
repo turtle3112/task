140c2c5dce47669f70bf53d15712f132af991299
linh <turtlenink@gmail.com>
2025-07-05 14:00:31 +0700

    1

--- task-manager-web-0f35fa89a2518e881bd0a44763d12d7279ea8375 (1)/task-manager-web-0f35fa89a2518e881bd0a44763d12d7279ea8375/src/main/java/com/vti/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vti.repository;
-
-import com.vti.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
-}
